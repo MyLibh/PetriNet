f6fa09ca0b99d3904cd981aeb9e590826de36cbb
mylibh <gav503244@gmail.com>
2020-12-26 00:56:55 +0300

    Graphics base added

---
+++ GraphicsImpl/src/main.cpp
@@ -0,0 +1,23 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <SFML/Graphics.hpp>
+
+auto main() -> int
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "PetriNet Visualization");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
